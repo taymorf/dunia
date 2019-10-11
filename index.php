40bfa1003ada0cbd8f58926ba6a7682d3dd179fb
taymorf <tayhum@gmail.com>
2019-10-11 17:12:20 +0200

    w

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <title></title>
   </head>
   <body>
-
+    <form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+   Name: <input type="text" name="fname">
+   <input type="submit" value="Submit">
+   </form> 
   </body>
 </html>
