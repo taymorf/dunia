9938fcea8fe01c219002d19e19d8c13b8a722282
taymorf <tayhum@gmail.com>
2019-10-11 17:10:31 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+<script src="https://cdn.jsdelivr.net/tamorf/dunia/sanitize.js"></script>
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
