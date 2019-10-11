9da93c235da4651df0fccd519f69c9e94b322d87
taymorf <tayhum@gmail.com>
2019-10-12 01:29:03 +0200

    s

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <title></title>
-  </head>
-  <body>
-    <h1>My First Web Page</h1>
-  <p>My First Paragraph</p>
-
-  <p id="demo"></p>
+<?php
+include_once("controller/UserController.php");
+$controller = new UserController();
+$controller->invoke();
+?>
 <script src="https://cdn.jsdelivr.net/gh/taymorf/dunia/sanitize.js"> </script>
-  </body>
-</html>
