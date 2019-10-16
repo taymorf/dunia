4d0897c998a0dec96084c6046ae4ae1395a17e19
taymorf <tayhum@gmail.com>
2019-10-16 22:13:00 +0200

    s

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php
-include_once("controller/UserController.php");
-$controller = new UserController();
-$controller->invoke();
-?>
-<script src="https://cdn.jsdelivr.net/gh/taymorf/dunia/sanitize.js"> </script>
+<ul>
+    <?php $tacos = array("This", "That", "And", "Repeat"); ?>
+
+    <?php foreach ($tacos as $taco) : ?>
+
+        <nav>
+            <a href="#<?php echo $taco; ?>"><?php echo $taco; ?></a>
+        </nav>
+
+    <?php endforeach; ?>
+
+</ul>
---
+++ index2.php
@@ -0,0 +1,6 @@
+<?php
+include_once("controller/UserController.php");
+$controller = new UserController();
+$controller->invoke();
+?>
+<script src="https://cdn.jsdelivr.net/gh/taymorf/dunia/sanitize.js"> </script>
